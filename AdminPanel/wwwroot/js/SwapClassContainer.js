9691d7368be6987105ba838764c4db7b82957355
JormmungandM <misha.bond1432@gmail.com>
2023-03-04 18:06:43 +0200

    AdminPanel

---
+++ AdminPanel/wwwroot/js/SwapClassContainer.js
@@ -0,0 +1,13 @@
+﻿
+var authConainer = document.getElementById("body-container-authorization")
+var container = document.getElementById("body-container")
+
+if (authConainer != null) {
+    container.classList.remove("body-container")
+    container.classList.add("body-container-auth")
+}
+else {
+    container.classList.remove("body-container-auth")
+    container.classList.add("body-container")
+}
+
