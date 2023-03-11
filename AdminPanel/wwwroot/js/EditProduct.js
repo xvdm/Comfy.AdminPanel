8fa34aa65ec00c8bcef850ebe18c5025b7f546f8
JormmungandM <misha.bond1432@gmail.com>
2023-03-11 17:36:30 +0200

    Test view

--- AdminPanel/wwwroot/js/EditProduct.js
+++ AdminPanel/wwwroot/js/EditProduct.js
@@ -1,14 +1,3 @@
-﻿document.getElementById("btn-header").onclick = function () {
-    document.getElementById("btnMainForm").click()
-}
-
-let categoriesSelect = document.getElementById("categories-select")
-let divSubcategories = document.getElementById("subcategories-div")
-categoriesSelect.addEventListener("change", elementUpdate);
-
-if (categoriesSelect.options[categoriesSelect.selectedIndex].value != -1) {
-    divSubcategories.classList.remove("h-hidden")
-}
-
+﻿
 
 
