a1448b881071d457a79496b258d1a15d49042d70
tapeex <1lavabox1@gmail.com>
2023-03-23 16:45:02 +0200

    Сделал создание товара: Сделал страницу и обновил код создания в обработчике запроса mediatr

--- AdminPanel/MediatorHandlers/Products/CreateProductCommand.cs
+++ AdminPanel/MediatorHandlers/Products/CreateProductCommand.cs
@@ -10,9 +10,9 @@ namespace AdminPanel.Handlers.Products
     {
         public string Name { get; set; } = null!;
         public int Price { get; set; }
-        public string Brand { get; set; } = null!;
-        public string Category { get; set; } = null!;
-        public string Model { get; set; } = null!;
+        public int Brand { get; set; }
+        public int Category { get; set; }
+        public int Model { get; set; }
         public string Description { get; set; } = null!;
     }
 
@@ -34,14 +34,14 @@ namespace AdminPanel.Handlers.Products
                 Price = request.Price
             };
 
-            var brand = await _context.Brands.Where(x => x.Name == request.Brand).FirstOrDefaultAsync(cancellationToken);
+            var brand = await _context.Brands.Where(x => x.Id == request.Brand).FirstOrDefaultAsync(cancellationToken);
             if(brand is null) throw new HttpRequestException($"There is no brand {request.Brand}");
 
-            var model = await _context.Models.Where(x => x.Name == request.Model).FirstOrDefaultAsync(cancellationToken);
+            var model = await _context.Models.Where(x => x.Id == request.Model).FirstOrDefaultAsync(cancellationToken);
             if(model is null) throw new HttpRequestException($"There is no model {request.Brand}");
 
             var category = await _context.Subcategories
-                .Where(x => x.Name == request.Category)
+                .Where(x => x.Id == request.Category)
                 .Include(x => x.UniqueBrands)
                 .FirstOrDefaultAsync(cancellationToken);
             if(category is null) throw new HttpRequestException($"There is no category {request.Brand}");
