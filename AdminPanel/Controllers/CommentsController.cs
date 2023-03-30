f9ab5fe5ce1cccd2bd445139ab645ff7b044b399
tapeex <1lavabox1@gmail.com>
2023-03-30 21:55:26 +0300

    Доабвил в контроллере Comments экшены для смены активности комментариев.

--- AdminPanel/Controllers/CommentsController.cs
+++ AdminPanel/Controllers/CommentsController.cs
@@ -41,5 +41,33 @@ namespace AdminPanel.Controllers
             };
             return View(viewModel);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeQuestionActivityStatus(int id, bool isActive)
+        {
+            await _mediator.Send(new ChangeQuestionActivityStatusCommand(id, isActive));
+            return Redirect("/Comments/InactiveComments");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeQuestionAnswerActivityStatus(int id, bool isActive)
+        {
+            await _mediator.Send(new ChangeQuestionAnswerActivityStatusCommand(id, isActive));
+            return Redirect("/Comments/InactiveComments");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeReviewActivityStatus(int id, bool isActive)
+        {
+            await _mediator.Send(new ChangeReviewActivityStatusCommand(id, isActive));
+            return Redirect("/Comments/InactiveComments");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeReviewAnswerActivityStatus(int id, bool isActive)
+        {
+            await _mediator.Send(new ChangeReviewAnswerActivityStatusCommand(id, isActive));
+            return Redirect("/Comments/InactiveComments");
+        }
     }
 }
