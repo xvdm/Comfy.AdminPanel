3c3e3abac303c3076ec974399c07cf8633b0ea65
tapeex <1lavabox1@gmail.com>
2023-06-15 12:44:44 +0300

    Deleted unnecessary files

--- AdminPanel/Areas/Identity/Data/AdminPanelUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace AdminPanel.Areas.Identity.Data;
-
-// Add profile data for application users by adding properties to the AdminPanelUser class
-public class AdminPanelUser : IdentityUser
-{
-}
-
